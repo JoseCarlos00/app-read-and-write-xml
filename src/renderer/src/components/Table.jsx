cf356824473f8838bf762aad0287b60c47c15f36
JoseCarlos00 <101861240+JoseCarlos00@users.noreply.github.com>
2025-05-11 23:31:24 -0600

    Feat: TableComponent: Crear comunicacion para editar contenido devolviendo un nuevo Objeto Shipment al componente VIewSummary

--- src/renderer/src/components/Table.jsx
+++ src/renderer/src/components/Table.jsx
@@ -1,53 +1,145 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import { Popconfirm, Table, Button, Flex } from 'antd';
+
 import './Table.css';
 import { useEditeContent } from '../hooks/editeContentTable';
 
-// import data from '../mock/data.json';
+class ShipmentDetail {
+  constructor({ erpOrder, sku, qty, lineNumber }) {
+    this.ErpOrderLineNum = lineNumber;
+    this.SKU = sku;
+    this.TotalQuantity = qty;
+    this.ErpOrder = erpOrder;
 
-const getContentBodyTable = (data) => {
+    this.example = {
+      Action: ['Save'],
+      ErpOrder: ['3405-32523'],
+      ErpOrderLineNum: ['646840'],
+      RequestedQty: ['36'],
+      SKU: [
+        {
+          Company: ['FM'],
+          Item: ['1025-3645-32152'],
+          Quantity: ['36'],
+          QuantityUm: ['PZ'],
+        },
+      ],
+      TotalQuantity: ['36'],
+    };
+  }
+
+  createDetail() {
+    return {
+      Action: ['Save'],
+      ErpOrder: [this.ErpOrder],
+      ErpOrderLineNum: [this.ErpOrderLineNum],
+      RequestedQty: [this.TotalQuantity],
+      SKU: [
+        {
+          Company: ['FM'],
+          Item: [this.SKU],
+          Quantity: [this.TotalQuantity],
+          QuantityUm: ['PZ'],
+        },
+      ],
+      TotalQuantity: [this.TotalQuantity],
+    };
+  }
+
+  static getNewArrayObject(shipmentDetails) {
+    return shipmentDetails.map((detail) => {
+      return new ShipmentDetail(detail).createDetail();
+    });
+  }
+}
+
+const getContentBodyTable = (shipmentDetails) => {
   try {
-    const shipmentDetails =
-      data.WMWROOT.WMWDATA[0].Shipments[0].Shipment[0].Details[0]
-        .ShipmentDetail;
-
-    if (!shipmentDetails) {
-      return [];
+    if (
+      !shipmentDetails ||
+      !Array.isArray(shipmentDetails) ||
+      shipmentDetails.length === 0
+    ) {
+      return []; // Devuelve un array vacío si no hay datos
     }
 
-    const content = shipmentDetails.map(
-      ({ SKU, TotalQuantity: Quantity, ErpOrderLineNum: LineNumber }) => {
-        const { Item } = SKU[0];
-        const key = LineNumber.toString();
+    const content = shipmentDetails
+      .map((detail) => {
+        const ErpOrderLineNum = detail?.ErpOrderLineNum;
+        const SKUData = detail?.SKU;
+        const TotalQuantity = detail?.TotalQuantity;
+        const ErpOrder = detail?.ErpOrder;
+
+        // Es crucial que LineNumber exista para la key
+        if (
+          typeof ErpOrderLineNum === 'undefined' ||
+          ErpOrderLineNum === null
+        ) {
+          console.warn('Detalle omitido por falta de ErpOrderLineNum:', detail);
+          return null; // Omitir este elemento si no tiene LineNumber
+        }
+
+        const key = ErpOrderLineNum.toString();
+
+        const skuValue =
+          SKUData &&
+          Array.isArray(SKUData) &&
+          SKUData.length > 0 &&
+          SKUData[0] &&
+          typeof SKUData[0].Item !== 'undefined'
+            ? SKUData[0].Item.toString()
+            : 'N/A'; // Valor por defecto o placeholder para SKU
+
+        const quantityValue =
+          typeof TotalQuantity !== 'undefined' ? TotalQuantity.toString() : '0'; // Valor por defecto
+
+        const erpOrderValue =
+          typeof ErpOrder !== 'undefined' ? ErpOrder.toString() : '0'; // Valor por defecto
+
+        if (erpOrderValue === '0') {
+          console.warn('Detalle omitido por falta de ErpOrder:', detail);
+          return null; // Omitir este elemento si no tiene LineNumber
+        }
 
         return {
           key,
-          sku: Item.toString(),
-          qty: Quantity.toString(),
+          sku: skuValue,
+          qty: quantityValue,
           lineNumber: key,
+          erpOrder: erpOrderValue,
         };
-      },
-    );
+      })
+      .filter((item) => item !== null); // Filtrar elementos que no pudieron ser procesados
 
     return content;
   } catch (error) {
-    console.log('Error al obtener el contenido de la tabla:', error);
-    return [];
+    console.error('Error al procesar los datos para la tabla:', error); // Usar console.error
+    return []; // Devuelve un array vacío en caso de error
   }
 };
 
-const TableComponent = ({ bodyContent }) => {
+const TableComponent = ({ tableContent, onContentChange }) => {
   const { EditableRow, EditableCell } = useEditeContent();
 
-  const [dataSource, setDataSource] = useState(
-    getContentBodyTable(bodyContent),
-  );
-
+  // Inicializar dataSource como un array vacío
+  const [dataSource, setDataSource] = useState([]);
   const [selectedRowKeys, setSelectedRowKeys] = useState([]);
+
+  useEffect(() => {
+    const tbodyContent = getContentBodyTable(tableContent);
+    setDataSource(tbodyContent);
+  }, [tableContent]); // setDataSource no necesita ser una dependencia
+
+  const handleOnContentChange = (newTableContent) => {
+    const newArrayObject = ShipmentDetail.getNewArrayObject(newTableContent);
+    console.log('newArrayObject', newArrayObject);
+    onContentChange(newArrayObject);
+  };
 
   const handleDelete = (key) => {
     const newData = dataSource.filter((item) => item.key !== key);
     setDataSource(newData);
+    handleOnContentChange(newData);
   };
 
   const defaultColumns = [
@@ -70,7 +162,7 @@ const TableComponent = ({ bodyContent }) => {
       title: 'Actions',
       dataIndex: 'operation',
       render: (_, record) =>
-        dataSource.length >= 1 ? (
+        dataSource && dataSource.length >= 1 ? (
           <Popconfirm
             title={`Eliminar el item: ${record.sku}?`}
             okText="Eliminar"
@@ -87,9 +179,11 @@ const TableComponent = ({ bodyContent }) => {
     const newData = [...dataSource];
     const index = newData.findIndex((item) => row.key === item.key);
     const item = newData[index];
-    newData.splice(index, 1, Object.assign(Object.assign({}, item), row));
+    newData.splice(index, 1, { ...item, ...row });
+
     console.log('newData', newData);
     setDataSource(newData);
+    handleOnContentChange(newData);
   };
 
   const components = {
@@ -104,7 +198,8 @@ const TableComponent = ({ bodyContent }) => {
       return col;
     }
 
-    return Object.assign(Object.assign({}, col), {
+    return {
+      ...col,
       onCell: (record) => ({
         record,
         editable: col.editable,
@@ -112,7 +207,7 @@ const TableComponent = ({ bodyContent }) => {
         title: col.title,
         handleSave,
       }),
-    });
+    };
   });
 
   const onSelectChange = (newSelectedRowKeys) => {
@@ -127,12 +222,26 @@ const TableComponent = ({ bodyContent }) => {
 
   const hasSelected = selectedRowKeys.length > 0;
 
+  const handleDeleteSelected = () => {
+    const newData = dataSource.filter(
+      (item) => !selectedRowKeys.includes(item.key),
+    );
+    setDataSource(newData);
+    handleOnContentChange(newData);
+    setSelectedRowKeys([]);
+  };
+
   return (
     <Flex gap="middle" vertical className="container-principal">
       <Flex align="center" gap="middle">
-        <Button type="primary" disabled={hasSelected === false}>
-          Eliminar
+        <Button
+          type="primary"
+          onClick={handleDeleteSelected}
+          disabled={!hasSelected}
+        >
+          Eliminar Seleccionados
         </Button>
+        {/* Texto del botón actualizado para mayor claridad */}
         {hasSelected ? `Selected ${selectedRowKeys.length} items` : null}
       </Flex>
       <Table
--- src/renderer/src/components/ViewSummary.jsx
+++ src/renderer/src/components/ViewSummary.jsx
@@ -1,11 +1,125 @@
+import { useEffect, useState } from 'react';
+import { useTabManagerStore } from '../store/viewStore';
 import TableComponent from './Table';
 
-function ViewSummary({ content }) {
-  console.log('ViewSummary', { content });
+const DEBOUNCE_DELAY = 500;
+
+const useEditedContent = ({
+  onContentChange,
+  setIsModified,
+  tabKey,
+  globalObject,
+}) => {
+  const [debounceTimeout, setDebounceTimeout] = useState(null);
+  const setFilesModified = useTabManagerStore(
+    (state) => state.setFilesModified,
+  );
+  const setModifiedTabState = useTabManagerStore(
+    (state) => state.setModifiedTabState,
+  );
+
+  // Limpiar el timeout cuando el componente se desmonte
+  useEffect(() => {
+    return () => {
+      if (debounceTimeout) {
+        clearTimeout(debounceTimeout);
+      }
+    };
+  }, [debounceTimeout]);
+
+  const buildObject = () => {
+    return 'buildObject XML String';
+  };
+
+  const handleModifiedChange = (content) => {
+    // setFilesModified(true);
+    setModifiedTabState(tabKey, { isModified: true });
+
+    console.log('handleModifiedChange:', {
+      content,
+      globalObject,
+    });
+
+    const newObjectGlobal = getObjectGlobal(globalObject.dataObject, content);
+    const xmlString = buildObject(newObjectGlobal);
+
+    // Parser Object -> xmlString
+    // onContentChange(xmlString); // -> Mandar cambios a la fuente de verdad
+    console.log('Modificar remoto:', xmlString);
+  };
+
+  const handleObjectContentChange = (newTableContent) => {
+    console.log('[handleObjectContentChange]:', newTableContent);
+    setIsModified(true);
+
+    if (debounceTimeout) clearTimeout(debounceTimeout);
+
+    setDebounceTimeout(
+      setTimeout(() => handleModifiedChange(newTableContent), DEBOUNCE_DELAY),
+    );
+  };
+
+  return { handleObjectContentChange };
+};
+
+const getObjectGlobal = (globalObject, content) => {
+  try {
+    // Agregar al objecto global los cambios de la tabla
+    globalObject.WMWROOT.WMWDATA[0].Shipments[0].Shipment[0].Details[0].ShipmentDetail =
+      content;
+
+    return [globalObject];
+  } catch (error) {
+    console.log('Error al obtener el contenido de la tabla:', error);
+    return {};
+  }
+};
+
+const createObjectGlobal = (dataObject) => {
+  try {
+    const shipmentsDetail = [
+      ...dataObject.WMWROOT.WMWDATA[0].Shipments[0].Shipment[0].Details[0]
+        .ShipmentDetail,
+    ];
+
+    // Limpiar `Array` de shipments de dataObject
+    dataObject.WMWROOT.WMWDATA[0].Shipments[0].Shipment[0].Details[0].ShipmentDetail.length = 0;
+
+    return { tableContent: shipmentsDetail, dataObject };
+  } catch (error) {
+    console.log('Error al obtener el contenido de la tabla:', error);
+    return {};
+  }
+};
+
+function ViewSummary({ content, onContentChange, setIsModified, tabKey }) {
+  // Objecto separado del contenido que va hacia los componentes hijos
+  // ObjectGlobal - Sin modificaciones
+  const [globalObject, setGlobalObject] = useState({});
+
+  const { handleObjectContentChange } = useEditedContent({
+    onContentChange,
+    setIsModified,
+    tabKey,
+    setGlobalObject,
+    globalObject,
+  });
+
+  useEffect(() => {
+    setGlobalObject(createObjectGlobal(content));
+  }, [content]);
+
+  console.log('ViewSummary', {
+    content,
+    tableContent: globalObject.tableContent,
+  });
 
   return (
     <div style={{ maxWidth: '700px', marginRight: 'auto', marginLeft: 'auto' }}>
-      <TableComponent bodyContent={content} />
+      <TableComponent
+        tableContent={globalObject.tableContent}
+        onContentChange={handleObjectContentChange}
+      />
     </div>
   );
 }
